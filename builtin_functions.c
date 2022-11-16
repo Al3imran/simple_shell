1d773fa224bd1c5473f92c19067b114d9c56d964
Al3imran <111256161+Al3imran@users.noreply.github.com>
2022-11-16 23:55:10 +0300

    1

--- builtin_functions.c
+++ builtin_functions.c
@@ -29,8 +29,7 @@ void quit(char **tokenized_command)
 {
 	int num_token = 0, arg;
 
-	for (; tokenized_command[num_token] != NULL; num_token++)
-		;
+	for (; tokenized_command[num_token] != NULL; num_token++);
 	if (num_token == 1)
 	{
 		free(tokenized_command);
